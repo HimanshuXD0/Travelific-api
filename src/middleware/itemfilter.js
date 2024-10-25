ad1e9bc70d1abfff4c50d6286ce51a99449afe15
CoderX <sharmahimanshu2429@gmail.com>
2024-10-25 15:32:15 +0530

    filter by email

--- src/middleware/itemfilter.js
+++ src/middleware/itemfilter.js
@@ -3,14 +3,14 @@
 
 export const applyFilters = (req, res, next) => {
  try {
-    const { minPrice, maxPrice, name, category } = req.query;
-    const {email}=req.body;
+    const { minPrice, maxPrice, name, category,email } = req.query;
     // Build dynamic filter object
     const filter = {};
 
     if (minPrice) filter.price = { ...filter.price, $gte: Number(minPrice) };
     if (maxPrice) filter.price = { ...filter.price, $lte: Number(maxPrice) };
-    if (email) filter.email = { $regex: new RegExp(name, 'i') }; // Case-insensitive search
+    //if (email) filter.email = { $regex: new RegExp(name, 'i') }; // Case-insensitive search
+    if (email) filter.email = email; // Case-insensitive search
     if (category) filter.category = category;
     // console.log(filter)
     req.filter = filter; 
