3c12a56381228f32da98f93e972732f4c634e7c9
CoderX <sharmahimanshu2429@gmail.com>
2025-06-16 09:31:55 +0530

    add update

--- src/middleware/itemfilter.js
+++ src/middleware/itemfilter.js
@@ -3,12 +3,12 @@
 
 export const applyFilters = (req, res, next) => {
  try {
-    const {email,pending} = req.query;
+    const {email,pending,noAction} = req.query;
     // Build dynamic filter object
     const filter = {};
     if (email && email!="admin123@gmail.com") filter.email = email; // Case-insensitive search
-    if (pending !== undefined) filter.pending = pending === 'true'; // converts string to boolean
-    if (noAction !== undefined) filter.pending = pending === 'true'; // converts string to boolean
+    // if (pending !== undefined) filter.pending = pending; // converts string to boolean
+    // if (noAction !== undefined) filter.pending = noAction; // converts string to boolean
     // console.log(filter)
     req.filter = filter; 
     //console.log(req)
